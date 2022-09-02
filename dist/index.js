7f11574f37c675b5c9234dd829fd9e4ba3b67ec6
caesar003 <caesarmuksid@gmail.com>
2022-09-02 10:18:33 +0700

    Basic types

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,17 @@
 "use strict";
-let id = 5;
-// id = '5';
-const nickName = "brad";
-var usage = null;
+// Basic Types
+let age = 42;
+let nickName = "Caesar";
+let isMarried = false;
+// we defined so, to ensure the data types are kept the way we want them to.
+/**
+ * we can also define a variable and it's datatype to use later
+ */
+let height;
+// then we use it
+height = 145;
+// height = true
+// => Error: Type 'boolean' is not assignable to type 'number'.
+// type "any" could be anything;
+let isComing = true;
+isComing = "may be";
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
+// Basic Types
 
-let id = 5;
+let age: number = 42;
+let nickName: string = "Caesar";
+let isMarried: boolean = false;
 
-// id = '5';
+// we defined so, to ensure the data types are kept the way we want them to.
 
-const nickName = "brad";
+/**
+ * we can also define a variable and it's datatype to use later
+ */
 
-var usage = null;
+let height: number
+
+// then we use it
+
+height = 145;
+
+// height = true
+// => Error: Type 'boolean' is not assignable to type 'number'.
+
+
+// type "any" could be anything;
+
+let isComing:any = true;
+isComing = "may be";
